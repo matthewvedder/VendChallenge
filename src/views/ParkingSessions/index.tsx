6ddbb299a8f65b463790ed9eba893c3b61ab64d8
Matthew <matthewvedder@gmail.com>
2023-10-30 10:00:24 -0400

    Create ParkingSessions view

---
+++ src/views/ParkingSessions/index.tsx
@@ -0,0 +1,13 @@
+// libraries
+import React from "react"
+
+// styles
+import './index.css'
+
+export default function ParkingSessions() {
+    return (
+        <div className="parking-sessions">
+            <h1>Parking Sessions</h1>
+        </div>
+    )
+}   
