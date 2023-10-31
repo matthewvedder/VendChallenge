289f8c3ea62a2b922423f596f83df160644bd925
Matthew <matthewvedder@gmail.com>
2023-10-30 22:43:40 -0400

    Use read only version of MUI data grid

--- src/views/ParkingSessions/DataGrid/index.tsx
+++ src/views/ParkingSessions/DataGrid/index.tsx
@@ -1,217 +1,96 @@
+// libraries
 import * as React from 'react';
+// components
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/DeleteOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import CancelIcon from '@mui/icons-material/Close';
-import { formatPhoneNumber } from '../../../util/format'
-import {
-  GridRowsProp,
-  GridRowModesModel,
-  GridRowModes,
-  DataGrid,
-  GridColDef,
-  GridColTypeDef,
-  GridToolbarContainer,
-  GridActionsCellItem,
-  GridEventListener,
-  GridRowId,
-  GridRowModel,
-  GridRowEditStopReasons,
-} from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+// utils
+import { formatPhoneNumber } from '../../../util/format/';
 
-const initialRows: GridRowsProp = [
+const columns: GridColDef[] = [
+  { field: 'licensePlateNumber', headerName: 'License Plate Number', width: 180, editable: true },
+  { 
+    field: 'phoneNumber', 
+    headerName: 'Phone Number', 
+    width: 180, 
+    valueFormatter: ({ value }) => formatPhoneNumber(value || ''),
+  },
+  {
+    field: 'enteredAt',
+    headerName: 'Entered At',
+    type: 'dateTime',
+    width: 180,
+  },
+  {
+    field: 'exitedAt',
+    headerName: 'Exited At',
+    type: 'dateTime',
+    width: 180,
+  },
+  {
+    field: 'status',
+    headerName: 'Status',
+    width: 220,
+  },
 ]
 
-interface EditToolbarProps {
-  setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
-  setRowModesModel: (
-    newModel: (oldModel: GridRowModesModel) => GridRowModesModel,
-  ) => void;
-}
+const rows = [
+  {
+    id: 1,
+    licensePlateNumber: 'ABC123',
+    phoneNumber: '12345678901',
+    enteredAt: new Date(),
+    exitedAt: new Date(),
+    status: 'active',
+  },
+  {
+    id: 2,
+    licensePlateNumber: 'DEF456',
+    phoneNumber: '12345678901',
+    enteredAt: new Date(),
+    exitedAt: new Date(),
+    status: 'completed',
+  },
+  {
+    id: 3,
+    licensePlateNumber: 'GHI789',
+    phoneNumber: '12345678901',
+    enteredAt: new Date(),
+    exitedAt: new Date(),
+    status: 'active',
+  },
+  {
+    id: 4,
+    licensePlateNumber: 'JKL012',
+    phoneNumber: '12345678901',
+    enteredAt: new Date(),
+    exitedAt: new Date(),
+    status: 'completed',
+  },
+  {
+    id: 5,
+    licensePlateNumber: 'MNO345',
+    phoneNumber: '12345678901',
+    enteredAt: new Date(),
+    exitedAt: new Date(),
+    status: 'active',
+  }
+];
 
-const generateGuid = () => {
-  var S4 = function() {
-    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-  };
-  return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
-}
-
-function EditToolbar(props: EditToolbarProps) {
-  const { setRows, setRowModesModel } = props;
-
-  const handleClick = () => {
-    const randomId = generateGuid()
-    setRows((oldRows) => [...oldRows, { id: randomId, name: '', age: '', isNew: true }]);
-    setRowModesModel((oldModel) => ({
-      ...oldModel,
-      [randomId]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
-    }));
-  };
-
+export default function DataGridDemo() {
   return (
-    <GridToolbarContainer>
-      <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
-        Add parking session
-      </Button>
-    </GridToolbarContainer>
-  );
-}
-
-export default function FullFeaturedCrudGrid() {
-  const [rows, setRows] = React.useState(initialRows);
-  const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>({});
-
-  const handleRowEditStop: GridEventListener<'rowEditStop'> = (params, event) => {
-    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
-      event.defaultMuiPrevented = true;
-    }
-  };
-
-  const handleEditClick = (id: GridRowId) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
-  };
-
-  const handleSaveClick = (id: GridRowId) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-  };
-
-  const handleDeleteClick = (id: GridRowId) => () => {
-    setRows(rows.filter((row) => row.id !== id));
-  };
-
-  const handleCancelClick = (id: GridRowId) => () => {
-    setRowModesModel({
-      ...rowModesModel,
-      [id]: { mode: GridRowModes.View, ignoreModifications: true },
-    });
-
-    const editedRow = rows.find((row) => row.id === id);
-    if (editedRow!.isNew) {
-      setRows(rows.filter((row) => row.id !== id));
-    }
-  };
-
-  const processRowUpdate = (newRow: GridRowModel) => {
-    const updatedRow = { ...newRow, isNew: false };
-    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
-    return updatedRow;
-  };
-
-  const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
-    setRowModesModel(newRowModesModel);
-  };
-
-  
-  const phoneNumber: GridColTypeDef = {
-    width: 130,
-    valueFormatter: ({ value }) => formatPhoneNumber(value || ''),
-    cellClassName: 'font-tabular-nums',
-  };
-
-  const columns: GridColDef[] = [
-    { field: 'licensePlateNumber', headerName: 'License Plate Number', width: 180, editable: true },
-    { field: 'phoneNumber', headerName: 'Phone Number', width: 180, editable: true, ...phoneNumber },
-    {
-      field: 'enteredAt',
-      headerName: 'Entered At',
-      type: 'dateTime',
-      width: 180,
-      editable: true,
-    },
-    {
-      field: 'exitedAt',
-      headerName: 'Exited At',
-      type: 'dateTime',
-      width: 180,
-      editable: true,
-    },
-    {
-      field: 'status',
-      headerName: 'Status',
-      width: 220,
-      editable: true,
-      type: 'singleSelect',
-      valueOptions: ['active', 'completed'],
-    },
-    {
-      field: 'actions',
-      type: 'actions',
-      headerName: 'Actions',
-      width: 100,
-      cellClassName: 'actions',
-      getActions: ({ id }) => {
-        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
-
-        if (isInEditMode) {
-          return [
-            <GridActionsCellItem
-              icon={<SaveIcon />}
-              label="Save"
-              sx={{
-                color: 'primary.main',
-              }}
-              onClick={handleSaveClick(id)}
-            />,
-            <GridActionsCellItem
-              icon={<CancelIcon />}
-              label="Cancel"
-              className="textPrimary"
-              onClick={handleCancelClick(id)}
-              color="inherit"
-            />,
-          ];
-        }
-
-        return [
-          <GridActionsCellItem
-            icon={<EditIcon />}
-            label="Edit"
-            className="textPrimary"
-            onClick={handleEditClick(id)}
-            color="inherit"
-          />,
-          <GridActionsCellItem
-            icon={<DeleteIcon />}
-            label="Delete"
-            onClick={handleDeleteClick(id)}
-            color="inherit"
-          />,
-        ];
-      },
-    },
-  ];
-
-  return (
-    <Box
-      sx={{
-        height: 500,
-        marginTop: 10,
-        width: '100%',
-        '& .actions': {
-          color: 'text.secondary',
-        },
-        '& .textPrimary': {
-          color: 'text.primary',
-        },
-      }}
-    >
+    <Box sx={{ height: 400, width: '100%', marginTop: 10 }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        editMode="row"
-        rowModesModel={rowModesModel}
-        onRowModesModelChange={handleRowModesModelChange}
-        onRowEditStop={handleRowEditStop}
-        processRowUpdate={processRowUpdate}
-        slots={{
-          toolbar: EditToolbar,
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
         }}
-        slotProps={{
-          toolbar: { setRows, setRowModesModel },
-        }}
+        pageSizeOptions={[20, 50, 100]}
+        disableRowSelectionOnClick
       />
     </Box>
   );
